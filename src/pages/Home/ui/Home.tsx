cc3f65dbbefbc89cd165b28f43d120b61db605f4
Dmitry Kodolbenko <115988813+DmitryKodolbenko@users.noreply.github.com>
2024-12-18 09:26:07 +0400

    add new main page design

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-irregular-whitespace */
-import { Address } from "@ton/ton";
+import { motion } from "framer-motion";
 import queryString from "query-string";
 import { FC, useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -15,14 +15,24 @@ import { PrivateLayout } from "@/shared/layouts";
 import { TokenBalance } from "@/shared/lib/types/multichainAccount";
 import { TokenDetailQueryObj } from "@/shared/lib/types/token";
 import s from "./Home.module.scss";
+import { miniApp } from "@telegram-apps/sdk-react";
+import { SvgSelector } from "@/shared/lib/assets/svg-selector";
+import { SkeletonRect } from "@/shared/components/Skeletons";
+import { formatNumber } from "@/shared/lib/helpers/formatNumber";
+import { checkDesktopPlatform } from "@/shared/lib/helpers/checkDesktopPlatform";
+import { useOpenConnect } from "@/features/connect/model/connectService";
+import { useQRScanner } from "@/features/qrScanner";
 
 export const Home: FC = () => {
     const [tab, setActiveTab] = useState("main.navigation.assets");
 
     const tabs = ["main.navigation.assets", "main.navigation.nft"];
 
-    const { data: accountBalance, isFetching: accountBalanceFetching } =
-        useFetchTotalBalanceQuery();
+    const {
+        data: accountBalance,
+        isFetching: accountBalanceFetching,
+        refetch: refetchBalance
+    } = useFetchTotalBalanceQuery();
 
     const navigate = useNavigate();
     const handleTokenSelect = useCallback(
@@ -30,11 +40,11 @@ export const Home: FC = () => {
             const query: TokenDetailQueryObj = {
                 platform: token.platform,
                 tokenContract: token.tokenContract,
-                isNativeToken: token.isNativeToken,
+                isNativeToken: token.isNativeToken
             };
             const url = queryString.stringifyUrl({
                 url: "/token",
-                query: Object(query),
+                query: Object(query)
             });
             navigate(url);
         },
@@ -43,12 +53,75 @@ export const Home: FC = () => {
 
     useConnectEffect();
     useHandleConnectMessage();
-    // test();
+
+    useEffect(() => {
+        const previousBackgroundColor = document.body.style.backgroundColor;
+        document.body.style.backgroundColor = "#16161A";
+        miniApp.setHeaderColor("#16161A");
+
+        return () => {
+            document.body.style.backgroundColor = previousBackgroundColor;
+            miniApp.setHeaderColor("#1f1f25");
+        };
+    }, []);
+
+    const [rotation, setRotation] = useState<number>(0);
+
+    const handleReload = () => {
+        setRotation(rotation - 360);
+        refetchBalance();
+    };
+
+    const isDesktop = checkDesktopPlatform();
+    const { connect } = useOpenConnect();
+    const [scanHandle] = useQRScanner({ connect });
 
     return (
         <PrivateLayout>
             <div className={s.home}>
-                <MainWalletInfo />
+                {/* <MainWalletInfo /> */}
+
+                <div className={s.top}>
+                    <motion.button
+                        className={s.icon_button}
+                        animate={{ rotate: rotation }}
+                        transition={{ duration: 0.6 }}
+                        onClick={handleReload}
+                        disabled={accountBalanceFetching}
+                    >
+                        <SvgSelector id="reload" />
+                    </motion.button>
+
+                    <button className={s.icon_button} disabled={isDesktop} onClick={scanHandle}>
+                        <SvgSelector id="qr-code-2" />
+                    </button>
+                </div>
+
+                <div className={s.balance}>
+                    <div className={s.balanceLabel}>Total balance</div>
+                    <div className={s.balanceText}>
+                        {accountBalanceFetching ? (
+                            <SkeletonRect height={40} width={200} />
+                        ) : (
+                            "$" + formatNumber(String(accountBalance?.totalUSDBalance))
+                        )}
+                    </div>
+                </div>
+
+                <div className={s.actions}>
+                    <div className={s.actionsButton} onClick={() => navigate("/send")}>
+                        <div className={s.actionsIcon}>
+                            <SvgSelector id="arrow-up" />
+                        </div>
+                        <div className={s.actionsText}>Send</div>
+                    </div>
+                    <div className={s.actionsButton} onClick={() => navigate("/receive")}>
+                        <div className={s.actionsIcon}>
+                            <SvgSelector id="arrow-down" />
+                        </div>
+                        <div className={s.actionsText}>Receive</div>
+                    </div>
+                </div>
 
                 {/* <AlertMessage
                         btnText="Показать секретную фразу"
@@ -57,12 +130,12 @@ export const Home: FC = () => {
                     /> */}
 
                 <div className={s.homeContent}>
-                    <TabSelector
+                    {/* <TabSelector
                         disabled={accountBalanceFetching}
                         activeTab={tab}
                         setActiveTab={setActiveTab}
                         tabs={tabs}
-                    />
+                    /> */}
                     {tab === "main.navigation.assets" && (
                         <TokensList
                             search
