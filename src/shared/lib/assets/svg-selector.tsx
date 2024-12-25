e29b730a1f954613ed1bbf0a935517b9ea4598b0
anovic123 <anovicjosip@gmail.com>
2024-12-25 19:14:10 +0200

    f

--- src/shared/lib/assets/svg-selector.tsx
+++ src/shared/lib/assets/svg-selector.tsx
@@ -1293,6 +1293,24 @@ export const SvgSelector: FC<SvgSelectorProps> = ({ id, isTg }) => {
                     />
                 </svg>
             );
+        case "checked-purple":
+            return (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                >
+                    <path
+                        d="M3.125 11.25L7.5 15.625L17.5 5.625"
+                        stroke="#A999F8"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            );
         default:
             return null;
     }
