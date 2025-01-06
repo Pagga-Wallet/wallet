9bf6847037fb74b44451ce529bd91ccedfd6f347
Makar Pustovalov <makar.pht@gmail.com>
2025-01-06 22:51:01 +0300

    solana network

--- src/shared/api/solana/lib/providers/solanaRPC.ts
+++ src/shared/api/solana/lib/providers/solanaRPC.ts
@@ -1,4 +1,4 @@
 import { Connection } from "@solana/web3.js";
 
-const net = "devnet";
+const net = import.meta.env.VITE_NETWORK === "MAINNET" ? "mainnet" : "devnet";
 export const solanaRPC = new Connection(`https://api.${net}.solana.com`, "confirmed");
