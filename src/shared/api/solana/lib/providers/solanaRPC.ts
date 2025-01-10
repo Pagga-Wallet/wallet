3ad3a79c16b46ac6fd202bab996a790491d8f49a
Makar Pustovalov <makar.pht@gmail.com>
2025-01-11 02:01:28 +0300

    fix rpc

--- src/shared/api/solana/lib/providers/solanaRPC.ts
+++ src/shared/api/solana/lib/providers/solanaRPC.ts
@@ -1,4 +1,7 @@
 import { Connection } from "@solana/web3.js";
 
-const net = import.meta.env.VITE_NETWORK === "MAINNET" ? "mainnet" : "devnet";
-export const solanaRPC = new Connection(`https://api.${net}.solana.com`, "confirmed");
+const rpcUrl =
+    import.meta.env.VITE_NETWORK === "MAINNET"
+        ? "https://solana-api.instantnodes.io/token-" + import.meta.env.VITE_SOLANA_RPC_API_KEY
+        : "https://api.devnet.solana.com";
+export const solanaRPC = new Connection(rpcUrl, "confirmed");
